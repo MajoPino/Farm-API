9f037559cf53b0c98ba79f41f15144ec33313fdf
MajoPino <majo12pino@gmail.com>
2024-09-17 07:09:40 -0500

    fix: Configuration of the AnimalType class

--- Models/AnimalType.cs
+++ Models/AnimalType.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Farm_API.Models;
 
+[Table("animal_types")]
 public class AnimalType
 {
+    [Column("id")]
     public int Id { get; set; }
+
+    [Column("name")]
     public required string Name { get; set; } // Mammal, Bird, Reptile
+    
+    [Column("description")]
     public required string Description { get; set; }
 }
